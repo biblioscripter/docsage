92cbe39d623d8a174b1a488f34f3e8d166e75a3d
Jameel Ahmad <jameelahmad221b@gmail.com>
2024-07-12 15:13:56 +0500

    Create copy_images_to_folders.py

---
+++ copy_images_to_folders.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+from glob import glob
+
+def get_subfolder_names(path):
+    subfolders = glob(os.path.join(path, '*/'))  
+    subfolder_names = [os.path.basename(os.path.normpath(subfolder)) for subfolder in subfolders]
+    return subfolders, subfolder_names 
+
+def create_folders_from_names(base_dir, subfolder_names):
+    for name in subfolder_names:
+        new_folder_path = os.path.join(base_dir, name)
+        if not os.path.exists(new_folder_path):  
+            os.makedirs(new_folder_path)
+            print(f"Created folder: {new_folder_path}")
+        else:
+            print(f"Folder already exists: {new_folder_path}")
+
+def copy_images_to_folders(subfolders, base_dir):
+    for subfolder in subfolders:
+        subfolder_name = os.path.basename(os.path.normpath(subfolder))
+        destination_folder = os.path.join(base_dir, subfolder_name)
+        for file in glob(os.path.join(subfolder, "*")):
+            if file.endswith((".png", ".jpg", ".jpeg")):  
+                shutil.copy2(file, destination_folder)  
+
+folder_path = 'train' 
+base_dir = '.'         
+
+subfolders, subfolder_names = get_subfolder_names(folder_path)
+create_folders_from_names(base_dir, subfolder_names)
+copy_images_to_folders(subfolders, base_dir)
